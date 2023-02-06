bf007f6058626b35b6858d8e291e326dfed0f49b
Suraj Vijay <surajcpp115@gmail.com>
2023-02-06 19:54:31 +0530

    restyled code

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+
 const combineMiddlewares = require("./utils/middleware");
 const {app} = require("./utils/server");
-
 combineMiddlewares(app);
+
+const userRoute = require("./routes/user");
+app.use("/api/user", userRoute);
+
+// base route
+app.get("/", (req, res) => {
+    return res.status(200).json("Base route for CACG API");
+});
+
+// all other invalid routes
+app.get("/*", (req, res) => {
+    res.redirect('/');
+});
 
 const PORT = process.env.port || 5000;
 app.listen(PORT, async () => {
---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const User = require("../models/user");
+
+// check username presence
+router.get("/:username", async (req, res) => {
+    
+});
+
+module.exports = router;
