ca4cdf92f8ea6daecf4c3375dc3e944fbd743102
Suraj Vijay <surajcpp115@gmail.com>
2023-02-07 06:15:47 +0530

    creating profile

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,22 @@ const router = require("express").Router();
 const User = require("../models/user");
 
 // check username presence
-router.get("/:username", async (req, res) => {
+router.get("/username/:username", async (req, res) => {
     const {username} = req.params;
     try {
         const doc = await User.findOne({username});
+        if (!doc) return res.status(200).json({presence: false});
+        return res.status(200).json({ presence: true });
+    } catch (err) {
+        return res.status(500).json(JSON.stringify(err));
+    }
+});
+
+// check account presence
+router.get("/account/:accountId", async (req, res) => {
+    const {accountId} = req.params;
+    try {
+        const doc = await User.findOne({accountId});
         if (!doc) return res.status(200).json({presence: false});
         return res.status(200).json({ presence: true });
     } catch (err) {
