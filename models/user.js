b112a7a447e14b8aca56b228d0cb2b7ae7d4259a
Suraj Vijay <surajcpp115@gmail.com>
2023-02-08 20:42:00 +0530

    updated user model to have likes

--- models/user.js
+++ models/user.js
@@ -18,6 +18,10 @@ const UserSchema = new mongoose.Schema({
     },
     reputation: {
         type: Number,
+        default: 1
+    },
+    likes: {
+        type: Number,
         default: 0
     },
     isCommunity: {
