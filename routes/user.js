e7e8d373cd826aec6c3f62475e69abf411364ec7
Suraj Vijay <surajcpp115@gmail.com>
2023-02-09 22:24:55 +0530

    route to update username and name

--- routes/user.js
+++ routes/user.js
@@ -100,4 +100,17 @@ router.get("/user-rp/:accountId", async (req, res) => {
     }
 });
 
+// edit user profile
+router.put("/user-edit/:accountId", async (req, res) => {
+    try {
+        await User.findOneAndUpdate(
+            {accountId: req.params.accountId},
+            {$set: req.body}
+        );
+        return res.status(200).json("Account has been uupdated");
+    } catch (err) {
+        return res.status(500).json(JSON.stringify(err));
+    }
+});
+
 module.exports = router;
