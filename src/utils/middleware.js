c06cc2dbe2c0f4356508f27e1722a776b57c892c
Suraj Vijay <surajcpp115@gmail.com>
2023-02-16 22:22:54 +0530

    removed cors

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -4,14 +4,15 @@ const cors = require("cors");
 
 const morganConfig = require("./morgan");
 
-const corsConfig = {
-    optionsSuccessStatus: 200,
-    origin: "https://subdot.netlify.app"
-};
+// const corsConfig = {
+//     optionsSuccessStatus: 200,
+//     origin: "https://subdot.netlify.app"
+// };
 
 const combineMiddlewares = (app) => {
-    if (process.env.NODE_ENV === "dev") app.use(cors());
-    else app.use(cors(corsConfig));
+    // if (process.env.NODE_ENV === "dev") 
+    app.use(cors());
+    // else app.use(cors(corsConfig));
     app.use(morgan(morganConfig));
     app.use(express.json());
 };
