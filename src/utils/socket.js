0f4d1913f40d9f9a02ef5abdfcdfbdfa3d7c8485
Suraj Vijay <surajcpp115@gmail.com>
2023-02-17 10:59:32 +0530

    realtime advertisement

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,3 +1,26 @@
-const socketHandler = io => {};
+const socketHandler = io => {
+    io.on("connection", socket => {
+        console.log(`[SOCKET] ${socket.id} Connected`);
+        
+        socket.on("disconnect", () => {
+            console.log(`[SOCKET] ${socket.id} Disconnected`);
+        });
+
+        socket.on("joinRoom", roomId => {
+            socket.join(roomId);
+            console.log(`[SOCKET] ${socket.id} joined ${roomId}`);
+        });
+
+        socket.on("leaveRoom", roomId => {
+            socket.leave(roomId);
+            console.log(`[SERVER] ${socket.id} left ${roomId}`);
+        });
+
+        socket.on("newAdvert", advert => {
+            socket.broadcast.to("advert").emit("newAdvert", advert);
+            console.log(`[SOCKET] ${socket.id} published advertisement with id: ${advert._id}`);
+        });
+    });
+};
 
 module.exports = socketHandler;
