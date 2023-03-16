633a33a533694c27fbc5bb4442ec69c52bc7c71e
Suraj Vijay <surajcpp115@gmail.com>
2023-03-17 00:15:58 +0530

    migrated txs from mongodb to cassandra

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -1,13 +1,18 @@
 const router = require("express").Router();
-const Transaction = require("../models/transaction");
+const cqlClient = require("../utils/astra");
 
 // add new transaction to user
 router.post("/new", async (req, res) => {
     const {body: tx} = req;
     try {
-        const newTx = new Transaction(tx);
-        const doc = await newTx.save();
-        return res.status(200).json(doc);
+        const QUERY = `
+            INSERT INTO transactions 
+            (account_id, created_at, amount, description, kind)
+            VALUES (?, toTimeStamp(now()), ?, ?, ?);
+        `;
+        const VALUE = [tx.accountId, tx.amount, tx.desc, tx.kind];
+        await cqlClient.execute(QUERY, VALUE, {prepare: true});
+        return res.status(200).json("Transaction inserted");
     } catch (err) {
         return res.status(500).json(JSON.stringify(err));
     }
@@ -15,14 +20,32 @@ router.post("/new", async (req, res) => {
 
 // fetch all transactions by accountId
 router.post("/:accountId", async (req, res) => {
-    const {accountId} = req.params;
     try {
-        const skip = req.body?.skip || 0;
-        const docs = await Transaction.find({accountId})
-        .sort({ createdAt: "descending" })
-        .skip(skip)
-        .limit(7);
-        return res.status(200).json(docs);
+        const {accountId} = req.params;
+        const currPage = req.body.page || null;
+        const QUERY = `
+            SELECT account_id, created_at, amount,
+            description, kind FROM transactions
+            WHERE account_id = ? 
+            ORDER BY created_at DESC;
+        `;
+        const VALUE = [accountId];
+        const data = await cqlClient.execute(QUERY, VALUE, {
+            pageState: currPage,
+            fetchSize: 14
+        });
+        const txs = data.rows || [];
+        return res.status(200).json({
+            txs: txs.map(tx => ({
+                accountId: tx.account_id,
+                desc: tx.description,
+                kind: tx.kind,
+                amount: Number(tx.amount),
+                createdAt: tx.created_at,
+                _id: tx.created_at
+            })),
+            page: data.pageState
+        });
     } catch (err) {
         return res.status(500).json(JSON.stringify(err));
     }
