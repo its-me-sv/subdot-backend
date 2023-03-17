2f267c686b016d1754355896adb3699ca12c9d5c
Suraj Vijay <surajcpp115@gmail.com>
2023-03-17 12:26:17 +0530

    migrated advertisements to cassandra from mongodb

--- src/routes/advert.js
+++ src/routes/advert.js
@@ -1,16 +1,37 @@
 const router = require("express").Router();
+const {types} = require("cassandra-driver");
+const cqlClient = require("../utils/astra");
 const Advert = require("../models/advert");
 
 // add new advertisement
 router.post("/new", async (req, res) => {
     try {
-        const {expires, ...rest} = req.body;
-        const newAdvert = new Advert({
-            ...rest,
-            expires
-        });
-        const doc = await newAdvert.save();
-        return res.status(200).json(doc);
+        const { accountId, picture, link, expires } = req.body;
+        const QUERY = `
+            INSERT INTO advertisements 
+            (created_at, account_id, picture, link, expires) 
+            VALUES (?, ?, ?, ?, ?) USING TTL ?;
+        `;
+        const adId = types.TimeUuid.now().toString();
+        const expSec = Math.floor((new Date(expires).getTime() - Date.now()) / 1000);
+        const VALUE = [
+            adId, 
+            accountId, 
+            picture, 
+            link, 
+            expires, 
+            expSec
+        ];
+        await cqlClient.execute(QUERY, VALUE, {prepare: true});
+        const resBody = {
+            accountId,
+            picture,
+            link,
+            expires,
+            _id: adId,
+            __v: 0
+        };
+        return res.status(200).json(resBody);
     } catch (err) {
         return res.status(500).json(JSON.stringify(err));
     }
