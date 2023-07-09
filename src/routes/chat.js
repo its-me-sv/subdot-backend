7963e54fa0cf899743540efa9792268240a6668b
Suraj Vijay <surajcpp115@gmail.com>
2023-07-09 21:26:24 +0530

    altered schema to manage message verification

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -2,27 +2,48 @@ const router = require("express").Router();
 const { types } = require("cassandra-driver");
 const cqlClient = require("../utils/astra");
 
+// new message between two users
 router.post("/:sender/:reciever/:ipfs_id", async (req, res) => {
     try {
         const {sender, reciever, ipfs_id} = req.params;
         const chatId = [sender, reciever].sort().join("###");
         const message_id = types.TimeUuid.now().toString();
         const query = `
-            INSERT INTO messages(chat_id, message_id, ipfs_content_id)
-            VALUES (?, ?, ?);
+            INSERT INTO messages(chat_id, message_id, ipfs_content_id, verified)
+            VALUES (?, ?, ?, false);
         `;
         const values = [chatId, message_id, ipfs_id];
         await cqlClient.execute(query, values, {prepare: true});
         return res.status(200).json({
             message_id,
             created_at: new Date().toISOString(),
-            ipfs_content_id: ipfs_id
+            ipfs_content_id: ipfs_id,
+            verified: false
         });
     } catch (err) {
         return res.status(500).json(JSON.stringify(err));
     }
 });
 
+// update verification status
+router.put("/:sender/:reciever/:msgId", async (req, res) => {
+    try {
+        const { sender, reciever, msgId } = req.params;
+        const chatId = [sender, reciever].sort().join("###");
+        const query = `
+            UPDATE messages
+            SET verified = true
+            WHERE chat_id = ? AND message_id = ?;
+        `;
+        const param = [chatId, msgId];
+        await cqlClient.execute(query, param, {prepare: true});
+        return res.status(200).json("SUCCESS");
+    } catch (err) {
+        return res.status(500).json(JSON.stringify(err));
+    }
+});
+
+// fetch messages between two users
 router.post("/:sender/:reciever", async (req, res) => {
     try {
         const {sender, reciever} = req.params;
