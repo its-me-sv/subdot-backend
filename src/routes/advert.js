3240f3b9ca9a0ca0e55221e99b48edf2945891da
Suraj Vijay <surajcpp115@gmail.com>
2023-07-17 23:31:03 +0530

    endpoint to post advertisement

--- src/routes/advert.js
+++ src/routes/advert.js
@@ -5,15 +5,16 @@ const cqlClient = require("../utils/astra");
 // add new advertisement
 router.post("/new", async (req, res) => {
     try {
-        const { accountId, picture, link, expires } = req.body;
-        const QUERY = `
+        const { accountId, picture, link, expires, startedAt } = req.body;
+        // inserting into advertisements table
+        let QUERY = `
             INSERT INTO advertisements 
             (created_at, account_id, picture, link, expires) 
             VALUES (?, ?, ?, ?, ?) USING TTL ?;
         `;
-        const adId = types.TimeUuid.now().toString();
+        const adId = types.TimeUuid.fromDate(new Date(startedAt)).toString();
         const expSec = Math.floor((new Date(expires).getTime() - Date.now()) / 1000);
-        const VALUE = [
+        let VALUE = [
             adId, 
             accountId, 
             picture, 
@@ -23,35 +24,19 @@ router.post("/new", async (req, res) => {
         ];
         await cqlClient.execute(QUERY, VALUE, {prepare: true});
         const resBody = {
-            accountId,
+            created_at: adId,
+            account_id: accountId,
             picture,
             link,
-            expires,
-            _id: adId,
-            __v: 0
+            expires
         };
-        return res.status(200).json(resBody);
-    } catch (err) {
-        return res.status(500).json(JSON.stringify(err));
-    }
-});
-
-// fetch advertisement
-router.get("/", async (req, res) => {
-    try {
-        const QUERY = `SELECT * FROM advertisements LIMIT 1;`;
-        const data = await cqlClient.execute(QUERY);
-        const advert = data.rows[0];
-        if (!data.rowLength || !advert) 
-            return res.status(200).json(null);
-        const resBody = {
-            accountId: advert.account_id,
-            picture: advert.picture,
-            link: advert.link,
-            expires: advert.expires,
-            _id: advert.created_at,
-            __v: 0
-        };
+        // inserting into user_adverts table
+        QUERY = `
+            INSERT INTO user_adverts (account_id, advert_id)
+            VALUES (?, ?);
+        `;
+        VALUE = [accountId, adId];
+        await cqlClient.execute(QUERY, VALUE, { prepare: true });
         return res.status(200).json(resBody);
     } catch (err) {
         return res.status(500).json(JSON.stringify(err));
